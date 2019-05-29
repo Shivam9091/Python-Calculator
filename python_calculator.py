ff93ca982d68d4f6d37f3ab9423c6cca5d3d8e54
Khaliq Ansari <khaliqansari771@gmail.com>
2019-05-29 22:45:21 +0530

    Add files via upload

---
+++ python_calculator.py
@@ -0,0 +1,25 @@
+def add(a, b):
+    return a+b
+def mul(a, b):
+    return a*b
+def sub(a, b):
+    return a-b
+def adiv(a, b):
+    return a/b
+
+print ("select options: \n 1 For addition \n 2 for multiply \n 3 for sub \n 4 for divition")
+
+choice = int(input("Enter Your Choice 1,2,3,4"))
+num1 = float(input("Enter First number: "))
+num2 = float(input("Enter Second number: "))
+
+if choice == 1:
+    print("addition of {0} and {1} is {2}".format(num1, num2, add(num1,num2)))
+elif choice == 2:
+    print("multiplication of {0} and {1} is {2}".format(num1, num2, mul(num1,num2)))
+elif choice == 3:
+    print("subtraction of {0} and {1} is {2}".format(num1, num2, sub(num1,num2)))
+elif choice == 4:
+    print("division of {0} and {1} is {2}".format(num1, num2, div(num1,num2)))
+else:
+    print("Invalid input")
